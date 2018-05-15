2d3fc57fb6fe8c7dbfac6ab0ed0f1f26f03029eb
Andrew Holt <15341747+ajh1143@users.noreply.github.com>
2018-05-14 20:26:32 -0700

    Update FibonacciScatter.py

--- FibonacciScatter.py
+++ FibonacciScatter.py
@@ -3,6 +3,7 @@ import numpy as np
 plt.style.use('ggplot')
 
 class FibonacciScatter(object):
+  
     def FibonacciSequence(self, length):
         """
         :param length:  how many points to calculate
@@ -15,9 +16,6 @@ class FibonacciScatter(object):
             p1,p2 = p2, p1+p2
         return fibList
 
-
-
-
     def ScatterSequence(self, seq):
         """
         :param seq: List of fibonacci data points
@@ -29,11 +27,12 @@ class FibonacciScatter(object):
            x.append(position)
            y.append(value)
         plt.scatter(x,y)
-        z = np.polyfit(x, y, 1)
-        p = np.poly1d(z)
-        plt.plot(x, p(x), "r--")
+        fit = np.polyfit(x, y, 1)
+        y1d = np.poly1d(fit)
+        plt.plot(x, y1d(x), "r--")
         plt.show()
 
-x = FibonacciScatter()
-z=x.FibonacciSequence(10)
-x.ScatterSequence(z)
+if __name__ == "__main__":
+    x = FibonacciScatter()
+    data = x.FibonacciSequence(10)
+    x.ScatterSequence(data)
